76cef50c3c0b89a762f2fec5614ce582abf62612
MokoneSA <simonlephotojr@gmail.com>
2022-09-13 06:42:51 -0700

    0. _putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Program prints _putchar
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char name = "_putchar";
+
+	putchar(name);
+
+	return (0);
+}
