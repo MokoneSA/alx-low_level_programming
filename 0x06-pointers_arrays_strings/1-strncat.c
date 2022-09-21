19d0b46a158a6c101df4f69c4468fd29e9ceb24c
MokoneSA <simonlephotojr@gmail.com>
2022-09-21 18:09:10 +0000

    1. strncat

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -3,18 +3,18 @@
 
 /**
  * _strncat - concatenates two strings
- * @dest - string to be appended on
- * @src - string to be concatenated upon
- *
+ * @dest: string to be appended on
+ * @src: string to be concatenated upon
+ * @n: fixes the buffer overrun problem
  * Return: @dest
  */
 char *_strncat(char *dest, char *src, int n);
 {
-	char dest[15] = "Hello ";
-	char src[10] = "World!\n";
-	int n = dest + src;
+	int i;
 
-	_putchar(n);
+	for (i = 0 ; i < n && src[i] != '\0' ; i++)
+	       dest[i] = src[i];
+	dest[i] = '\0';
 
 	return(dest);
 }
