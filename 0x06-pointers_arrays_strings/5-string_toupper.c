95c74d3fcf696ec0feab976a22474731a39ee6fd
MokoneSA <simonlephotojr@gmail.com>
2022-09-22 04:55:24 +0000

    5. Always look up

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * string_toupper - changes lowercases letters to uppercase
+ * @str: parameter
+ * Return: returns a character
+ */
+char *string_toupper(char *str)
+{
+	int index = 0;
+
+	while (str[index++])
+	{
+		if (str[index] >= 'a' && str[index] <= 'z')
+			str[index] -= 32;
+	}
+	return (str);
+}
