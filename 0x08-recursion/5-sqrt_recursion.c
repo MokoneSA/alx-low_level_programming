4bdb8a6e9f16aa4d5c0833dbd4c7249cce7c5e94
MokoneSA <simonlephotojr@gmail.com>
2022-09-28 18:56:59 +0000

    5. Your subconscious is looking for the dreamer

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -10,7 +10,7 @@ int _sqrt_recursion(int n)
 {
 	if (n == 0 || n == 1)
 		return (n);
-	return (n);
+	return (_sqrt(0, n));
 }
 
 /**
@@ -26,5 +26,5 @@ int _sqrt(int n, int x)
 		return (-1);
 	else if (n * n == x)
 		return (n);
-	return (n + 1);
+	return (_sqrt(n + 1, x));
 }
