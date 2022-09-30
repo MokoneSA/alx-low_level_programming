069bf0c468d74f89486a110501dd1f6d78bfc714
MokoneSA <simonlephotojr@gmail.com>
2022-09-30 13:23:51 +0000

    0. A library is not a luxury but one of the necessities of life

--- 0x09-static_libraries/0-memset.c
+++ 0x09-static_libraries/0-memset.c
@@ -1,5 +1,13 @@
 #include "main.h"
-
+/**
+* _memeset - the memset() function fills
+* the first n bytes of the memory area
+* pointed to by s with the constant byte b
+* @s:target
+* @b: constant byte
+* @n:number of byte
+* Return: returns new value of target
+*/
 
 char *_memset(char *s, char b, unsigned int n)
 {
