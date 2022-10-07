2f761e386789da907122ebaa596dcb79c2f66323
MokoneSA <simonlephotojr@gmail.com>
2022-10-07 09:21:01 +0000

    4. _realloc

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -20,9 +20,9 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 
 	if (new_size == 0 && ptr)
 	{
-			free(ptr);
-			return (NULL);
-		}
+		free(ptr);
+		return (NULL);
+	}
 
 	if (!ptr)
 		return (malloc(new_size));
@@ -35,15 +35,15 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 
 	if (new_size < old_size)
 	{
-			for (i = 0; i < new_size; i++)
-				ptr1[i] = old_ptr[i];
-		}
+		for (i = 0; i < new_size; i++)
+		ptr1[i] = old_ptr[i];
+	}
 
 	if (new_size > old_size)
 	{
-			for (i = 0; i < old_size; i++)
-				ptr1[i] = old_ptr[i];
-		}
+		for (i = 0; i < old_size; i++)
+		ptr1[i] = old_ptr[i];
+	}
 
 	free(ptr);
 	return (ptr1);
