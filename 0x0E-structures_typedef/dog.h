b4539a697d281f1949b206c3ee1149bbcf836291
MokoneSA <simonlephotojr@gmail.com>
2022-10-10 15:09:54 +0000

    0. Poppy

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -10,6 +10,14 @@ typedef struct dog dog_t;
 dog_t *new_dog(char *name, float age, char *owner);
 void free_dog(dog_t *d);
 
+
+/**
+ * struct dog - a new type of data
+ * representing a dog
+ * @name: name of dog
+ * @age: age of dog
+ * @owner: owner's name
+ */
 struct dog
 {
 char *name;
